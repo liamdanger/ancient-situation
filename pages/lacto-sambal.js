9e83598f965925169f16628cf37137d16f4fe6ac
Liam Campbell <hi@liamcampbell.info>
2018-12-23 21:01:23 -0800

    Change font, nix hero image

--- pages/lacto-sambal.js
+++ pages/lacto-sambal.js
@@ -4,7 +4,6 @@ import Hero from '../components/hero'
 const Page = ({ title }) => (
     <Layout title="Lacto-Sambal">
         <h1>Lacto-Sambal</h1>
-        <Hero image="/static/lacto-sambal.jpg" caption="Look at this delicious shit" />
         <p>As the name may imply, this is a lacto-fermented sambal oelek-like chili paste.</p>
     </Layout>
 )
