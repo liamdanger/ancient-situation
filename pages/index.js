d1487f1a3b60a5a1b773e765ab8b843df8ad0669
Liam Campbell <hi@liamcampbell.info>
2019-01-13 13:45:59 -0800

    Some lacto copy

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import ThingList from '../components/thing-list'
 
 import things from '../things'
 
-const Index = () => ( 
+const Index = () => (
     <Layout title="Home">
-        <p><strong>Ancient Situation</strong> a &ldquo;boutique&rdquo; <span class="quiet">(lol)</span> label for food and drinks made by <a href="http://liamcampbell.info">Liam Campbell</a>. That&rsquo;s me.</p>
+        <p><strong>Ancient Situation</strong> a &ldquo;boutique&rdquo; <span className="quiet">(lol)</span> label for food and drinks made by <a href="http://liamcampbell.info">Liam Campbell</a>. That&rsquo;s me.</p>
         <p>Everything is extremely limited-run, since it has to come out of my little San Francisco kitchen. Nothing is FDA-approved.</p>
         <p>This site documents the stuff I&rsquo;ve made under the <strong>Ancient Situation</strong> name from 2018 on.</p>
         <ThingList things={things} />
--- pages/lacto-sambal.js
+++ pages/lacto-sambal.js
@@ -4,7 +4,15 @@ import Hero from '../components/hero'
 const Page = ({ title }) => (
     <Layout title="Lacto-Sambal">
         <h1>Lacto-Sambal</h1>
-        <p>As the name may imply, this is a lacto-fermented sambal oelek-like chili paste.</p>
+        <p>As the name may imply, this is a lacto-fermented sambal-like chili paste. Despite containing a fuck-ton of chili peppers, it's not <em>that</em> spicy, and there's a gratifying amount of lemongrass and garlic that comes through. I was kind of surprised by how well balanced it turned out, and I wonder how much of that is due to the funky lacto-fermentation flavors curbing the spiciness.</p>
+        <p>Grandma-ass recipe follows:</p>
+        <h2>Ingredients</h2>
+        <ul>
+            <li>A big pile of red jalapeños, probably like 30–40.</li>
+            <li>Two heads of garlic, peeled and crushed</li>
+            <li>A big 'ol knob of ginger, peeled</li>
+            <li>Three or so stalks of lemongrass, cut into inch-and-a-half long segments</li>
+        </ul>
     </Layout>
 )
 
